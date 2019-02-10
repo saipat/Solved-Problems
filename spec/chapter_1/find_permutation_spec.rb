6c90b42c679759587dbdcc4d87711de2b252294b
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-02-10 07:31:13 -0800

    Add permutations spec condition

--- spec/chapter_1/find_permutation_spec.rb
+++ spec/chapter_1/find_permutation_spec.rb
@@ -4,4 +4,8 @@ describe 'Find Permutations' do
     it 'returns the permutations of the given array' do
         expect(permutation([1,2,3])).to eq([[1, 2, 3], [2, 1, 3], [2, 3, 1], [1, 3, 2], [3, 1, 2], [3, 2, 1]])
     end
+
+    it 'returns the array if array length is than or equal to 1' do
+        expect(permutation([1])).to eq([[1]])
+    end
 end
