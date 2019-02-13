9d5769fb24419d082ae61cbd8208558ebf37ad7a
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-02-13 08:50:13 -0800

    Rephrase hacker rank array rotation

--- lib/chapter_1/array_rotation.rb
+++ lib/chapter_1/array_rotation.rb
@@ -1,5 +1,7 @@
 #  *** Write a method that is functionally equivalent to the rotate(offset) method of arrays. offset=1 ensures that the value of offset is 1 if no argument is provided. 
 
+# *** HackerRank - Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+
 def my_rotate(array, offset = 1)
     shift = offset % array.length
     array.drop(shift) + array.take(shift)
