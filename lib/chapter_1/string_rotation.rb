906777074c3f1e99e6c212feacb59ae2e3c0af9c
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-02-13 08:34:24 -0800

    Solve string rotation

--- lib/chapter_1/string_rotation.rb
+++ lib/chapter_1/string_rotation.rb
@@ -1,1 +1,17 @@
-# *** Write code to rotate a string
+# *** Write code to rotate a string.
+
+def rotated_string?(s1, s2)
+    is_match = false
+
+    return false unless s1.length == s2.length
+
+    count = 0
+    
+    until count == s1.length
+        temp = s1.split('')
+        s1 = temp.push(temp.shift).join('')
+        return true if s1 == s2
+        count += 1
+    end
+
+end
--- spec/chapter_1/string_rotation_spec.rb
+++ spec/chapter_1/string_rotation_spec.rb
@@ -1,3 +1,19 @@
 require "chapter_1/string_rotation.rb"
 
-describe "String "
+describe "String Rotation" do
+   it "returns true if s1 is a rotation of s2" do
+        expect(rotated_string?('waterbottle', 'erbottlewat')).to eq(true)
+    end
+
+    it "returns true if s1 is a rotation of s2" do
+        expect(rotated_string?('likespuzzle', 'zzlelikespu')).to eq(true)
+    end
+
+    it "return false if s1 is not a rotation of s2" do
+        expect(rotated_string?('codinginterview', 'incointerviewg')).to eq(false)
+    end
+
+    it "return false if s1 length is not euqal to s2" do
+        expect(rotated_string?('bootcamp','camp')).to eq(false)
+    end
+end
