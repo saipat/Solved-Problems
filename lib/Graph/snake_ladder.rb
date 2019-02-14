4dfc71e78caf5d50175773f93c5df724d14f4a0f
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-02-14 14:01:58 -0800

    Rename

---
+++ lib/Graph/snake_ladder.rb
@@ -0,0 +1,4 @@
+#  *** Given a snake and ladder board, find the minimum number of dice throws required to reach the destination or last cell from source or 1st cell. Basically, the player has total control over outcome of dice throw and wants to find out minimum number of throws required to reach last cell.
+
+# If the player reaches a cell which is base of a ladder, the player has to climb up that ladder and if reaches a cell is mouth of the snake, has to go down to the tail of snake without a dice throw.
+
