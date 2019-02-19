f3a35a8c8e26f8e255d576dfbd82b8b75a6a4398
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-02-19 05:09:35 -0800

    Add time and space complexities

--- lib/chapter_2/remove_dups.rb
+++ lib/chapter_2/remove_dups.rb
@@ -3,7 +3,7 @@ require_relative 'linked_list'
 # *** Write code to remove duplicates from unsorted linked list.
 # How would you solve the problem if the temprorary buffer is not allowed?
 
-# with temprorary buffer
+# with temprorary buffer => O(n)time and O(n) extra space will be used in the Set if all characters are unique
 def remove_dups(list)
     return nil if list.nil? || list.head.next.nil?
     
