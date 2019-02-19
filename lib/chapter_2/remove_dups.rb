1806c5cddaf084fb3eb309f88deac7859951a74a
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-02-19 06:12:45 -0800

    Use hash to remove dup

--- lib/chapter_2/remove_dups.rb
+++ lib/chapter_2/remove_dups.rb
@@ -4,22 +4,42 @@ require_relative 'linked_list'
 # How would you solve the problem if the temprorary buffer is not allowed?
 
 # with temprorary buffer => O(n)time and O(n) extra space will be used in the Set if all characters are unique
+# def remove_dups(list)
+#     return nil if list.nil? || list.head.next.nil?
+    
+#     current = list.head
+
+#     set = Set.new
+
+#     until current.nil?
+#         set << current.value
+
+#         if !current.next.nil? && set.include?(current.next.value)
+#             current.next = current.next.next
+#         end
+
+#         current = current.next
+#     end
+
+#     set.to_a
+# end
+
+
+# with hash
 def remove_dups(list)
     return nil if list.nil? || list.head.next.nil?
-    
+
+    seen = {}
     current = list.head
 
-    set = Set.new
-
-    until current.nil?
-        set << current.value
-
-        if !current.next.nil? && set.include?(current.next.value)
-            current.next = current.next.next
+    while current
+        if seen[current.value]
+            current = current.next
+        else 
+            seen[current.value] = true
+            current = current.next
         end
-
-        current = current.next
     end
 
-    set.to_a
+    seen.keys
 end
--- spec/chapter_2/remove_dups_spec.rb
+++ spec/chapter_2/remove_dups_spec.rb
@@ -3,11 +3,34 @@ require 'chapter_2/remove_dups'
 
 describe "Remove Duplicates" do
 
-    # let(:input){ Node.new('a', Node.new('b', Node.new('a', Node.new('c', nil))))}
-    # let(:output){ Node.new('a', Node.new('b', Node.new('c', nil)))}
+    it "removes the duplicates from the unsorted lists" do
+        @nodes = %w[a b c a d a]
+        @list = LinkedList.new(@nodes.first)
+
+        (@nodes.drop(1)).each do |node| 
+            @list.add(node)
+        end
+
+        @nodes = @nodes.uniq
+
+        expect(remove_dups(@list)).to eq(@nodes)
+    end
 
     it "removes the duplicates from the unsorted lists" do
-        @nodes = %w[a b c a d]
+        @nodes = %w[a a a]
+        @list = LinkedList.new(@nodes.first)
+
+        (@nodes.drop(1)).each do |node| 
+            @list.add(node)
+        end
+
+        @nodes = @nodes.uniq
+
+        expect(remove_dups(@list)).to eq(@nodes)
+    end
+
+    it "removes the duplicates from the unsorted lists" do
+        @nodes = %w[10 20 30 40 50]
         @list = LinkedList.new(@nodes.first)
 
         (@nodes.drop(1)).each do |node| 
