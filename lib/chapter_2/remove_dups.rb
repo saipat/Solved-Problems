2c06b7f9b6c20686ba40414ce3967502f4147a3a
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-02-19 05:08:38 -0800

    Solve remove_dups with buffer

--- lib/chapter_2/remove_dups.rb
+++ lib/chapter_2/remove_dups.rb
@@ -1,3 +1,25 @@
-# *** Write code to remove duplicates from unsorted linked list.
-# How would you solve the problem if the temprorary buff is not allowed?
+require_relative 'linked_list'
 
+# *** Write code to remove duplicates from unsorted linked list.
+# How would you solve the problem if the temprorary buffer is not allowed?
+
+# with temprorary buffer
+def remove_dups(list)
+    return nil if list.nil? || list.head.next.nil?
+    
+    current = list.head
+
+    set = Set.new
+
+    until current.nil?
+        set << current.value
+
+        if !current.next.nil? && set.include?(current.next.value)
+            current.next = current.next.next
+        end
+
+        current = current.next
+    end
+
+    set.to_a
+end
--- spec/chapter_2/remove_dups_spec.rb
+++ spec/chapter_2/remove_dups_spec.rb
@@ -1,10 +1,21 @@
+
 require 'chapter_2/remove_dups'
 
 describe "Remove Duplicates" do
-    let(:input){ Node.new('a', Node.new('b', Node.new('a', Node.new(c, nil))))}
-    let(:output){ Node.new('a', Node.new('b', Node.new('c', nil)))}
+
+    # let(:input){ Node.new('a', Node.new('b', Node.new('a', Node.new('c', nil))))}
+    # let(:output){ Node.new('a', Node.new('b', Node.new('c', nil)))}
 
     it "removes the duplicates from the unsorted lists" do
-        expect(remove_dups(input)).to eq(output)
+        @nodes = %w[a b c a d]
+        @list = LinkedList.new(@nodes.first)
+
+        (@nodes.drop(1)).each do |node| 
+            @list.add(node)
+        end
+
+        @nodes = @nodes.uniq
+
+        expect(remove_dups(@list)).to eq(@nodes)
     end
 end
