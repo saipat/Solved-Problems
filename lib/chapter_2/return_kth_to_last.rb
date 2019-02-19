ae0aa6165a408539caa67afc2f81770863df8b43
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-02-19 10:40:30 -0800

    Write the recursive approach and test spec 1 later

--- lib/chapter_2/return_kth_to_last.rb
+++ lib/chapter_2/return_kth_to_last.rb
@@ -3,34 +3,44 @@ require_relative 'linked_list'
 #  *** Implement an algorithm to find kth_to_last element of a singly linked list
 
 # simple and obvious solution, O(n) = n and O(1) space
-def kth_to_last(list, k)
-    return nil unless list
-    return list.head.value if list.count_nodes == k
+# def kth_to_last(list, k)
+#     return nil unless list
+#     return list.head.value if list.count_nodes == k
 
-    current = list.head
-    index = list.count_nodes - k
-    looked = 0
+#     current = list.head
+#     index = list.count_nodes - k
+#     looked = 0
 
-    until current.nil?
-        return current.value if looked == index
-        looked += 1
-        current = current.next
-    end
-end
+#     until current.nil?
+#         return current.value if looked == index
+#         looked += 1
+#         current = current.next
+#     end
+# end
 
-# 2 pointers approach. O(n) time and 0(1) space.
-def kth_to_last(list, k)
-    return nil if list.head.value == nil
+# # 2 pointers approach. O(n) time and 0(1) space.
+# def kth_to_last(node, k)
+#     return nil if node.value == nil
 
-    fast_cursor = list.head
-    slow_cursor = list.head
+#     fast_cursor = node
+#     slow_cursor = node
 
-    k.times { fast_cursor = fast_cursor.next }
+#     k.times { fast_cursor = fast_cursor.next }
 
-    until fast_cursor.nil?
-        fast_cursor = fast_cursor.next
-        slow_cursor = slow_cursor.next
-    end
+#     until fast_cursor.nil?
+#         fast_cursor = fast_cursor.next
+#         slow_cursor = slow_cursor.next
+#     end
 
-    slow_cursor.value
+#     slow_cursor.value
+# end
+
+# recursive approach
+
+def kth_to_last(node, k)
+    return 0 if node == nil
+    p node.next
+    index = kth_to_last(node.next, k) + 1;
+    
+    index == k ? node.value : nil
 end
--- spec/chapter_2/return_kth_to_last_spec.rb
+++ spec/chapter_2/return_kth_to_last_spec.rb
@@ -3,7 +3,7 @@ require 'chapter_2/return_kth_to_last'
 describe "Return Kth to the last" do
 
     it "returns 3rd to the last element" do
-        @nodes = %w[a b c d e f]
+        @nodes = %w[a b c d]
         @list = LinkedList.new(@nodes.first)
 
         (@nodes.drop(1)).each do |node| 
@@ -12,7 +12,7 @@ describe "Return Kth to the last" do
         
         p @list.display
 
-        expect(kth_to_last(@list, 3)).to eq('d')
+        expect(kth_to_last(@list.head, 2)).to eq('c')
     end
 
      it "returns head if list count == 1 and k = 1" do
@@ -25,13 +25,12 @@ describe "Return Kth to the last" do
 
         p @list.display
 
-        expect(kth_to_last(@list, 1)).to eq('a')
+        expect(kth_to_last(@list.head, 1)).to eq('a')
     end
 
     it "returns nil if there is no element in the list" do
-        @nodes = %w[]
-        @list = LinkedList.new(@nodes.first)
+        @list = LinkedList.new(nil)
 
-        expect(kth_to_last(@list, 4)).to eq(nil)
+        expect(kth_to_last(@list.head, 4)).to eq(nil)
     end
 end
