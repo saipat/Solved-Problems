9be35b18f04a98df89ba72cff1a74c6ac5b755b7
Sai Lakshmi Pattabiraman <sailakshmi.pattabiraman@gmail.com>
2019-02-26 05:35:49 -0800

    Solve Bubble sort with Prc and without Prc

---
+++ lib/search_and_sort/bubble_sort.rb
@@ -0,0 +1,54 @@
+class Array
+    # without prc
+    def bubble_sort_without_prc
+        return self if count <= 1
+        loop do
+            swapped = false
+
+            (length-1).times do |idx|
+                if self[idx] > self[idx+1]
+                    self[idx], self[idx+1] = self[idx+1], self[idx]
+                    swapped = true
+                end
+            end
+
+            break if !swapped
+        end
+
+        self
+    end
+
+    # with prc
+    def bubble_sort!(&prc)
+        prc = Proc.new{ |x,y| x <=> y }
+
+        loop do
+            swapped = false
+
+            (length-1).times do |idx|
+                if prc.call(self[idx], self[idx+1]) == 1
+                    self[idx], self[idx+1] = self[idx+1], self[idx]
+                    swapped = true
+                end
+            end
+
+            break if !swapped
+        end
+
+        self
+    end
+
+    # without modifying
+    def bubble_sort
+        self.dup.bubble_sort!
+    end
+end
+
+
+p [2,3,1,5,4].bubble_sort_without_prc
+p [2].bubble_sort_without_prc
+p [].bubble_sort_without_prc
+
+p [2,3,1,5,4].bubble_sort
+p [2].bubble_sort
+p [].bubble_sort
